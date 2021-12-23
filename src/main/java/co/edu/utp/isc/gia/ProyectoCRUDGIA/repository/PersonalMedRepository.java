0fd6a8d04cb762704fb13a4bb0c5da78325fcd41
Jose <jose.rivera@utp.edu.co>
2021-12-23 17:30:08 -0500

    solucion error en PersonalMedRepository

--- src/main/java/co/edu/utp/isc/gia/ProyectoCRUDGIA/repository/PersonalMedRepository.java
+++ src/main/java/co/edu/utp/isc/gia/ProyectoCRUDGIA/repository/PersonalMedRepository.java
@@ -1,6 +1,10 @@
 package co.edu.utp.isc.gia.ProyectoCRUDGIA.repository;
 
 
-public interface PersonalMedRepository {
+import co.edu.utp.isc.gia.ProyectoCRUDGIA.entities.PersonalMedEntity;
+import co.edu.utp.isc.gia.ProyectoCRUDGIA.service.impl.PersonalMedImpl;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonalMedRepository extends CrudRepository<PersonalMedEntity, Long>{
 
 }
