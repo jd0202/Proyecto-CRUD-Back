5a35b9f8117c04fe5850981ea15a488328c21426
Jose <jose.rivera@utp.edu.co>
2021-12-23 15:21:37 -0500

    first commit

---
+++ src/main/java/co/edu/utp/isc/gia/ProyectoCRUDGIA/ProyectoCrudGiaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.isc.gia.ProyectoCRUDGIA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCrudGiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoCrudGiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/utp/isc/gia/ProyectoCRUDGIA/ProyectoCrudGiaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.isc.gia.ProyectoCRUDGIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCrudGiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
